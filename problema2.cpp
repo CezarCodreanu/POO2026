81dc5e323d2da75d2d0deb19e1f751dbe14e91c8
Codreanu Cezar <cezar.codreanu06@gmail.com>
2026-02-17 10:33:04 +0200

    Update nou pb2

--- problema2.cpp
+++ problema2.cpp
@@ -8,8 +8,9 @@ int Div(int a, int b) { return a / b; }
 int main(int argc, char* argv[])
 {
     char input[] = "---H***E+++L+++L///O---P+++O/+-**O---";
-    func Operatori[6] = {Sum, Dif, 0, Mul, 0, Div};
+    func Operatori[6] = {Mul, Sum, 0, Dif, 0, Div};
     int S = 0, V;
+    cout << strlen(input) << endl;
     Content x = {0, 0};
     int idx = -1;
 
@@ -19,24 +20,24 @@ int main(int argc, char* argv[])
         switch (input[i] - 42)
         {
             case INMULTIRE:
-                idx = 2;
+                idx = 0;
                 x.p1 = 3;
-                x.p2 = 3;
+                x.p2 = 2;
                 break;
             case SUMA:
-                idx = 0;
-                x.p1 = 7;
+                idx = 1;
+                x.p1 = 15;
                 x.p2 = 5;
                 break;
             case DIFERENTA:
-                idx = 1;
-                x.p1 = 10;
+                idx = 3;
+                x.p1 = 12;
                 x.p2 = 1;
                 break;
             case IMPARTIRE:
-                idx = 3;
-                x.p1 = 8;
-                x.p2 = 4;
+                idx = 5;
+                x.p1 = 4;
+                x.p2 = 2;
                 break;
         }
 
