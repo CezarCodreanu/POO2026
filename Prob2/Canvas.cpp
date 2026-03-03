97cb4bbbbf092f8a24637f50783774bbd2d09ad1
Codreanu Cezar <cezar.codreanu06@gmail.com>
2026-03-03 08:31:36 +0200

    Canvas inceput

--- Prob2/Canvas.cpp
+++ Prob2/Canvas.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
 Canvas::Canvas(int width, int height){
+    matrix = new char*[height];
     for(int i=0; i < height; i++){
-        
+        matrix[i] = new char[width];
     }
+    Clear();
+}
+
+void Canvas::DrawLine(int x1, int y1, int x2, int y2, char ch){
+    //punctul de start / stop
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+
+    //pentru directia liniei
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+
+    int eroare = dx + dy;
 }
