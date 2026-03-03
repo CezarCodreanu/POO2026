0211de29fc428748953d123cd8837b8283e6ea21
Codreanu Cezar <cezar.codreanu06@gmail.com>
2026-03-03 08:52:17 +0200

    Bug

--- Prob2/Canvas.cpp
+++ Prob2/Canvas.cpp
@@ -11,6 +11,9 @@ Canvas::Canvas(int width, int height){
     Clear();
 }
 
+
+void SetPoint(int x, int y, char ch)
+
 void Canvas::DrawLine(int x1, int y1, int x2, int y2, char ch){
     //punctul de start / stop
     int dx = abs(x2 - x1);
@@ -20,5 +23,21 @@ void Canvas::DrawLine(int x1, int y1, int x2, int y2, char ch){
     int sx = (x1 < x2) ? 1 : -1;
     int sy = (y1 < y2) ? 1 : -1;
 
-    int eroare = dx + dy;
+    int eroare = dx + dy, e2;
+
+    while(true){
+        SetPoint(x1, y1, ch); // de facut
+        if(x1 == x2 && y1 == y2) //pana ajunge la punctul (x2, y2)
+        break;
+        e2 = 2*eroare;
+
+        if(e2>= dy){
+            eroare +=dy;
+            x1 += sx;
+        }
+        if(e2 <= dx){
+            eroare += dx;
+            y1 += sy;
+        }
+    }
 }
