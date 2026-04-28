15fc9cf3c3145f727415ac0b72d3443f760558e6
Codreanu Cezar <cezar.codreanu06@gmail.com>
2026-04-28 08:24:20 +0300

    Problema 1

--- Prob1/main.cpp
+++ Prob1/main.cpp
@@ -1,41 +1,19 @@
-#include <string>
-#include <fstream>
-#include <map>
-#include <queue>
-
-using namespace std;
-
-ifstream fin("input.txt");
-ofstream fout("output.txt");
+#include <bits/stdc++.h>
 
 int main()
 {
-    string text, separators = " ,.?!";
-    getline(fin, text);
-
-    map<string, int> words;
-
-    string word = "";
-    for (const char &ch : text) {
-        if (separators.find(ch) != string::npos) {
-            if (!word.empty()) {
-                words[word]++;
-                word.clear();
-            }       
-        }
-        else {
-            word += tolower(ch);
-        }
+    Map<int, const char *> m;
+    m[10] = "C++";
+    m[20] = "test";
+    m[30] = "Poo";
+    for (auto[key, value, index] : m)
+    {
+        printf("Index:%d, Key=%d, Value=%s\n", index, key, value);
     }
-
-    priority_queue<pair<int, string>, vector<pair<int,string>>, greater<pair<int,string>>> wordQueue;
-    for (const auto &it : words) {
-        wordQueue.push({-it.second, it.first});
-    }
-
-    while (!wordQueue.empty()) {
-        fout << wordQueue.top().second << " => " << -wordQueue.top().first << endl;
-        wordQueue.pop();
+    m[20] = "result";
+    for (auto[key, value, index] : m)
+    {
+        printf("Index:%d, Key=%d, Value=%s\n", index, key, value);
     }
     return 0;
 }
